2321e43e96bd38348f297b81a0cefd9e13cbfb88
FizzaChouhan <chouhanfizza@gmail.com>
2024-09-30 21:36:08 +0500

    completed

--- login.js
+++ login.js
@@ -33,3 +33,10 @@ lBtn.addEventListener('click', function() {
 
 
 
+//get logout btn through id
+var logoutBtn = document.getElementById('logout');
+//add event listener to logout btn
+logoutBtn.addEventListener('click', function(){
+    //on clicking go back to sign up page again
+    window.location.href = 'index.html';
+    });
---
+++ logout.js
@@ -0,0 +1,11 @@
+//get logout btn through id
+var logoutBtn = document.getElementById('logout');
+//add event listener to logout btn
+logoutBtn.addEventListener('click', function(){
+    //on clicking go back to sign up page again
+    window.location.href = 'index.html';
+    });
+
+
+
+
